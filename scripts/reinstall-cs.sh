f315d8bed88eee9684667bce8ed68452656a0ac2
sander <spostma@sphereon.com>
2018-12-12 14:19:42 +0000

    SPHEREON-442: Updated reinstall-cs.sh

--- scripts/reinstall-cs.sh
+++ scripts/reinstall-cs.sh
@@ -1,3 +1,6 @@
-cd /media/ramdisk/alfresco-content-app
-npm install ~/DEV/forked/alfresco-ng2-components/lib/dist/alfresco-adf-content-services-2.6.1.tgz
-cd ~/DEV/forked/alfresco-ng2-components
+$(dirname $0)/npm-build-all.sh
+cd $(dirname $0)/../lib/dist
+npm pack content-services/
+cd ../../../alfresco-content-app
+npm install ../alfresco-ng2-components/lib/dist/alfresco-adf-content-services-2.6.1.tgz
+cd ../alfresco-ng2-components
