850d6b2fcd06e9b834369e0c74d9aa6b23b7c1fb
sander <spostma@sphereon.com>
2018-12-13 14:59:50 +0000

    SPHEREON-442: Fixed registration time presentation

---
+++ scripts/reinstall-cs.sh
@@ -0,0 +1,6 @@
+$(dirname $0)/npm-build-all.sh
+cd $(dirname $0)/../lib/dist
+npm pack content-services/
+cd ../../../alfresco-content-app
+npm install ../alfresco-ng2-components/lib/dist/alfresco-adf-content-services-2.6.1.tgz
+cd ../alfresco-ng2-components
